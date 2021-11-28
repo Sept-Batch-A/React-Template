8fff15e5a1378da62dafb8c4fa352959bb397c5e
Abhishek Choudhary <abhi@Abhisheks-MacBook-Pro.local>
2021-11-28 10:49:59 +0530

    converted a html tempate to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import HeroSection from './Components/HeroSection/HeroSection'
+import FeatureSection from './Components/FeatureSection/FeatureSection'
+import CardSection from './Components/CardSection/CardSection'
+import Footer from './Components/Footer/Footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+          <Navbar />
+          <HeroSection />
+          <FeatureSection />
+          <CardSection />
+          <Footer />
+        </div>
+    )
 }
-
-export default App;
+export default App
